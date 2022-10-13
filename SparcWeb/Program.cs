909a004d92d58c91f266a32c32d0a9e695bf9e9b
Margaret Retzinger <maretzinger@gmail.com>
2022-10-13 16:29:58 -0400

    Refactoring and misc. frontend updates

--- SparcWeb/Program.cs
+++ SparcWeb/Program.cs
@@ -6,7 +6,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped<IbisContentProvider>();
+builder.Services.AddSingleton<IbisContentProvider>();
+builder.Services.AddHttpClient<IbisContentProvider>();
 builder.Services.AddApiAuthorization();
 builder.Sparcify();
 
