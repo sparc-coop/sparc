ba77d5c42adf75200e8412d8cdda73abbba257d5
Margaret Retzinger <maretzinger@gmail.com>
2022-11-07 17:44:44 -0500

    Updates for blog page and feature for SendEmailNotification

--- SparcFeatures/Program.cs
+++ SparcFeatures/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 
+//builder.Services.AddTwilio(Configuration);
+
 builder.Services.AddSingleton(sp =>
 {
     // Get the address that the app is currently running at
---
+++ SparcFeatures/SendEmailNotification.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace SparcFeatures
+{
+    public class SendEmailNotification : Feature<bool>
+    {
+        TwilioService Twilio { get; set; }
+
+        public SendRegistrationNotification(TwilioService twilio)
+        {
+            Twilio = twilio;
+        }
+
+        public override async Task<bool> ExecuteAsync(string emailAddress)
+        {
+            await Twilio.SendAsync(emailAddress, "Sparc email contact form");
+
+            return true;
+        }
+    }
+}
