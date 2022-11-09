6ace1d6f8879255a378a4daa529d4510fadfe5f6
Joseph Young <joseph@josephayoung.com>
2022-11-09 12:22:54 -0600

    Remove old SparcWeb project

--- SparcWeb/Program.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Sparc.Ibis;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddIbis();
-
-await builder.Build().RunAsync();
