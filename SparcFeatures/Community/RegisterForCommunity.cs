b78280c787551a4771703134c5682f83b32891b8
Joseph Young <joseph@josephayoung.com>
2022-11-09 11:34:34 -0600

    Converted Sparc to Blazor server!!!!!

---
+++ SparcFeatures/Community/RegisterForCommunity.cs
@@ -0,0 +1,21 @@
+﻿using Sparc.Kernel;
+using Sparc.Notifications.Twilio;
+
+namespace Sparc.Coop.Community;
+
+public record RegisterForCommunityRequest(string Email);
+public class RegisterForCommunity : Feature<RegisterForCommunityRequest, bool>
+{
+    TwilioService Twilio { get; set; }
+
+    public RegisterForCommunity(TwilioService twilio)
+    {
+        Twilio = twilio;
+    }
+
+    public override async Task<bool> ExecuteAsync(RegisterForCommunityRequest request)
+    {
+        await Twilio.AddContactAsync(request.Email, "689a1a54-cef8-4327-a04d-abd6a8787dd9");
+        return true;
+    }
+}
