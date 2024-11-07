1c2abd0552984053d13b89a250c8ecdd22196e44
Joseph Young <joseph@josephayoung.com>
2024-11-07 15:48:19 -0600

    Add passwordless auth to Sparc.Coop

--- SparcFeatures/Program.cs
+++ SparcFeatures/Program.cs
@@ -1,10 +1,13 @@
 using Sparc.Kori;
 using Sparc.Coop;
 using Sparc.Blossom;
+using Sparc.Blossom.Authentication.Passwordless;
+using Sparc.Blossom.Authentication;
 
 BlossomApplication.Run<Html>(args, builder =>
 {
     builder.AddKori(new Uri("https://sparc.coop"));
+    builder.AddBlossomPasswordlessAuthentication<BlossomUser>();
 },
 app =>
 {
