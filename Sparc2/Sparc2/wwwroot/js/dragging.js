c8082e44a2d768b193a6d06bdb4bc6d7e4a930d4
Christine Antonio <christinejm.antonio@gmail.com>
2025-03-21 13:15:20 -0700

    update nav and make fixed, change page size to test dragging feature

--- Sparc2/Sparc2/wwwroot/js/dragging.js
+++ Sparc2/Sparc2/wwwroot/js/dragging.js
@@ -3,22 +3,22 @@ let initialX, initialY;
 const scrollSpeed = 1;
 
 document.addEventListener('mousedown', (e) => {
-  isDragging = true;
-  initialX = e.clientX - window.scrollX;
-  initialY = e.clientY - window.scrollY;
-  document.body.style.cursor = 'grab';
+    isDragging = true;
+    initialX = e.clientX - window.scrollX;
+    initialY = e.clientY - window.scrollY;
+    document.body.style.cursor = 'grab';
 });
 
 document.addEventListener('mousemove', (e) => {
-  if (!isDragging) return;
-  const currentX = e.clientX;
-  const currentY = e.clientY;
-  window.scrollTo(scrollSpeed * (initialX - currentX), scrollSpeed * (initialY - currentY));
+    if (!isDragging) return;
+    const currentX = e.clientX;
+    const currentY = e.clientY;
+    window.scrollTo(scrollSpeed * (initialX - currentX), scrollSpeed * (initialY - currentY));
 });
 
 document.addEventListener('mouseup', () => {
-  isDragging = false;
-  document.body.style.cursor = 'default';
+    isDragging = false;
+    document.body.style.cursor = 'default';
 });
 
 document.addEventListener('mouseleave', () => {
