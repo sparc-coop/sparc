71ed38f8719ac7b0c1c46ca361565e597721e52e
Christine Antonio <christinejm.antonio@gmail.com>
2025-03-25 14:38:28 -0700

    installed gsap and starting code in scrolling.js -- scroll behavior is currently wonky

---
+++ Sparc2/Sparc2/wwwroot/js/scrolling.js
@@ -0,0 +1,37 @@
+﻿//gsap.registerPLugin(ScrollTrigger);
+
+window.addEventListener('scroll', () => {
+    var home = document.getElementsByClassName("home")[0];
+
+    if (home) {
+        gsap
+        .timeline({
+            scrollTrigger: {
+                trigger: ".home",
+                start: "top top",
+                end: "+=100%",
+                pin: true,
+                scrub: true,
+                //markers: true,
+            },
+        })
+        .to(".mosaic-container img", {
+            scale: 2,
+            z: 250,
+            transformOrigin: "center center",
+        })
+        //.to(".home", {
+        //    scale: 1.4,
+        //    boxShadow: '10000px 0 0 0 rgba(0,0,0,0.5) inset',
+        //    transformOrigin: "center center",
+        //},
+        //    "<"
+        //)
+        .to(".mosaic-container", {
+            autoAlpha: 0,
+        })
+        .to([".home, main"], {
+            height: 400,
+        });
+    }
+});
