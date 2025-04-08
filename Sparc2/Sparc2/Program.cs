51e6bd7618a6fd00b48434b67be3d862d1b29a99
Fernando Rodrigues <ferd-rodrigues@hotmail.com>
2025-04-08 10:01:27 -0300

    Integration with Blossom.Payment.Sparc, and added sample page "TestPayment"

--- Sparc2/Sparc2/Program.cs
+++ Sparc2/Sparc2/Program.cs
@@ -1,6 +1,12 @@
-﻿using Sparc2;
+﻿using Sparc.Blossom.Payment.Stripe;
+using Sparc2;
 
 var builder  = BlossomApplication.CreateBuilder(args);
+
+builder.Services.AddStripePayments(options =>
+{
+    options.ApiKey = builder.Configuration["Stripe:ApiKey"] ?? "sk_test_123";
+});
 
 var app = builder.Build();
 
