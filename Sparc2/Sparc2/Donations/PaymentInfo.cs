5e422ae23d18fa927532314ca34dacea133af472
Laura Ferraz <lauraferraz5@gmail.com>
2025-04-14 16:39:46 -0300

    Refactor nav menu to new specs, remove add card header, adjust styles and behavior

--- Sparc2/Sparc2/Donations/PaymentInfo.cs
+++ Sparc2/Sparc2/Donations/PaymentInfo.cs
@@ -8,12 +8,10 @@ public class PaymentInfo
     public string CVV { get; set; } 
     public string EmailAdress { get; set; }
     public string BillingAddress { get; set; }
-    public string City { get; set; }
-    public string ZipCode { get; set; }
     public string DonationId { get; set; }
     public UserDonation Donation { get; set; }
 
-    public PaymentInfo(string cardHolderName, string cardNumber, string expiryDate, string cvv, string emailAdress, string billingAddress, string city, string zipCode, string donationId)
+    public PaymentInfo(string cardHolderName, string cardNumber, string expiryDate, string cvv, string emailAdress, string billingAddress, string donationId)
     {
         CardHolderName = cardHolderName;
         CardNumber = cardNumber;
@@ -21,8 +19,6 @@ public class PaymentInfo
         CVV = cvv;
         EmailAdress = emailAdress;
         BillingAddress = billingAddress;
-        City = city;
-        ZipCode = zipCode;
         DonationId = donationId;
     }   
 }
