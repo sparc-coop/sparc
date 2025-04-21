845d94edb6c6a696e912e8f7e708199ad12824fa
Joseph Young <joseph@josephayoung.com>
2025-04-21 09:05:42 -0500

    Pull progress-based file upload up to Blossom and then back down to Sparc

--- Sparc2/Sparc2/Program.cs
+++ Sparc2/Sparc2/Program.cs
@@ -1,22 +1,10 @@
-﻿using Sparc.Blossom;
-using Sparc2;
-using Sparc2.Databases.AzureBlob;
+﻿using Sparc2;
 using Sparc2.Ideas;
+using Sparc.Blossom.Data;
 
 var builder  = BlossomApplication.CreateBuilder(args);
+builder.Services.AddAzureStorage(builder.Configuration);
 
-builder.Services.AddSingleton<AzureBlob>(sp =>
-{
-    var configuration = sp.GetRequiredService<IConfiguration>();
-    var connectionString = configuration.GetConnectionString("Storage");
-
-    if (string.IsNullOrEmpty(connectionString))
-    {
-        throw new InvalidOperationException("Azure Blob Storage connection string is not configured.");
-    }
-
-    return new AzureBlob(connectionString);
-});
 builder.Services.AddSingleton<IdeaService>();
 
 var app = builder.Build();
