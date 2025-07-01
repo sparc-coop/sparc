ffc465ba6a40ef1cd9de8d69fde5990dc8d900c5
Christine Antonio <christinejm.antonio@gmail.com>
2025-07-01 14:37:52 -0700

    menu icon animation using kute.js svgMorph

--- Sparc2/Sparc2/wwwroot/js/catalog.js
+++ Sparc2/Sparc2/wwwroot/js/catalog.js
@@ -92,3 +92,31 @@ function replaceURL(obj, url) {
     //history.replaceState(obj, "", url);
     window.history.back();
 }
+
+function hoverProductInView(dotnetReference, productId) {
+    const card = document.getElementById(productId);
+    let rect = card.getBoundingClientRect();
+    const viewportHeight = window.innerHeight;
+    const viewportWidth = window.innerWidth;
+
+    let visibleHeight = 0;
+    let visibleWidth = 0;
+    let visiblePercentage = 0;
+
+    let containerHeight = ((viewportHeight * .4) - 64);
+    let containerWidth = viewportWidth - 32;
+
+    if (rect.top < containerHeight && rect.bottom > 0) {
+        // Element is at least partially visible
+        visibleHeight = Math.min(rect.bottom, containerHeight) - Math.max(rect.top, 0);
+        visibleWidth = Math.min(rect.right, containerWidth) - Math.max(rect.left, 0);
+
+        const visiblePercentage = (visibleHeight * visibleWidth) / (rect.height * rect.width) * 100;
+        console.log(`Visible percentage: ${visiblePercentage}%`);
+
+        if (visiblePercentage >= 40) {
+            console.log("Product is more than 40% visible, show product info.");
+            //hoverProduct(dotnetReference, productId);
+        }
+    }
+}
---
+++ Sparc2/Sparc2/wwwroot/js/morph.js
@@ -0,0 +1,37 @@
+﻿function morphMenuSvg(morphForward) {
+    console.log('morphForward:', morphForward);
+    if (morphForward) {
+        const morph1 = KUTE.fromTo('#menu1A',
+            { path: '#menu1A' },
+            { path: '#menu3A' },
+            { duration: 1500, easing: 'easingCubicOut', morphPrecision: 1 });
+        const morph2 = KUTE.fromTo('#menu1B',
+            { path: '#menu1B' },
+            { path: '#menu3B' },
+            { duration: 1500, easing: 'easingCubicOut', morphPrecision: 1 });
+        const morph3 = KUTE.fromTo('#menu1C',
+            { path: '#menu1C' },
+            { path: '#menu3C' },
+            { duration: 1500, easing: 'easingCubicOut', morphPrecision: 1 });
+
+        !morph1.playing && morph1.start();
+        !morph2.playing && morph2.start();
+        !morph3.playing && morph3.start();
+    } else {
+        const morph1 = KUTE.fromTo('#menu1A',
+            { path: '#menu1A' },
+            { path: '#menu2A' },
+            { duration: 1500, easing: 'easingCubicOut', morphPrecision: 1 });
+        const morph2 = KUTE.fromTo('#menu1B',
+            { path: '#menu1B' },
+            { path: '#menu2B' },
+            { duration: 1500, easing: 'easingCubicOut', morphPrecision: 1 });
+        const morph3 = KUTE.fromTo('#menu1C',
+            { path: '#menu1C' },
+            { path: '#menu2C' },
+            { duration: 250, easing: 'easingCubicOut', morphPrecision: 1 });
+        !morph1.playing && morph1.start();
+        !morph2.playing && morph2.start();
+        !morph3.playing && morph3.start();
+    }
+}
