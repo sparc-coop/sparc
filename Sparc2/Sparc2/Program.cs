c093b1c0b7fcd6c57698006fa7b442cd1688c1fc
Laura Ferraz <lauraferraz5@gmail.com>
2025-07-12 02:17:56 -0300

    Start working on authentication flow in profile component

--- Sparc2/Sparc2/Program.cs
+++ Sparc2/Sparc2/Program.cs
@@ -1,13 +1,23 @@
-﻿using Sparc2;
-using Sparc.Blossom.Data;
+﻿using Sparc.Blossom.Data;
 using Sparc.Engine;
+using Sparc2;
 using Sparc2.Ideas;
 using Sparc2.Products;
+using System.Net;
 
 var builder = BlossomApplication.CreateBuilder<Html>(args);
 
 builder.Services.AddAzureStorage(builder.Configuration);
 builder.Services.AddSparcEngine();
+
+builder.Services.AddHttpClient("AuthService", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7185/");
+}).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler{});
+
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("AuthService")
+);
 
 builder.Services.AddSingleton<IdeaService>();
 
@@ -20,4 +30,4 @@ await ideaRepository.AddAsync(ProjectIdea.Generate(20));
 var productRepository = scope.ServiceProvider.GetRequiredService<IRepository<Product>>();
 await productRepository.AddAsync(Product.Generate(10));
 
-await app.RunAsync<Html>(); 
+await app.RunAsync<Html>();
