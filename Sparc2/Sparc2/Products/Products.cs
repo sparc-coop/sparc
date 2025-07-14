e210d21df89f4d8606a5ae6dc3d3ddf814ce7ce9
Joseph Young <joseph@josephayoung.com>
2025-07-14 11:03:47 -0500

    Add currency and order management features
    Introduced `SparcCurrency`, `SparcOrder`, and `SparcPaymentIntent` classes to enhance currency and payment processing. Updated `ProductPurchase` components to utilize the new currency structure and modified the `ISparcBilling` interface to return `SparcPaymentIntent`. Adjusted `StripePaymentService` for the new order structure and updated API documentation. Removed `UserPrice` class and refined currency conversion logic in `ExchangeRates.cs`. CSS updates for improved UI.

--- Sparc2/Sparc2/Products/Products.cs
+++ Sparc2/Sparc2/Products/Products.cs
@@ -2,7 +2,6 @@
 
 namespace Sparc2.Products;
 
-public record ProductInfo(Product Product, UserPrice Price);
 public class Products(BlossomAggregateOptions<Product> options) : BlossomAggregate<Product>(options)
 {
     public BlossomQuery<Product> GetAllProducts()
--- Sparc2/Sparc2/Products/UserPrice.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sparc2.Products;
-
-public class UserPrice(decimal amount, string currency)
-{
-    public decimal Amount { get; set; } = amount;
-    public SparcCurrency Currency { get; set; } = SparcCurrency.From(currency);
-}
