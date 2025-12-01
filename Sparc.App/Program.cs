82bad816487998db4478a83c1cd09a8808c6f33f
Joseph Young <joseph@josephayoung.com>
2025-12-01 09:28:33 -0600

    Combine AddBlossomEngine methods

--- Sparc.App/Program.cs
+++ Sparc.App/Program.cs
@@ -2,10 +2,9 @@ using Sparc.App;
 using Sparc.App.Layout;
 using Sparc.Blossom;
 using Sparc.Blossom.Data;
-using Sparc.Blossom.Engine;
 
 var builder = BlossomApplication.CreateBuilder<Html>(args);
-builder.Services.AddBlossomEngine("https://localhost:7185");
+builder.AddBlossomEngine("https://localhost:7185");
 builder.Services.AddCosmos<SparcContext>(builder.Configuration);
 
 await builder.Build().RunAsync<Html>();
