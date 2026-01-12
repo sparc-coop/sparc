6faf39a992f3246c048fd8baa01cf96a60b5046c
Joseph Young <joseph@josephayoung.com>
2026-01-12 15:24:21 -0600

    Initial game structure

---
+++ Sparc.App/wwwroot/game.js
@@ -0,0 +1,75 @@
+﻿class SparcGame extends Phaser.Scene {
+    height = 500;
+    width = 1280;
+
+    preload() {
+        this.load.setBaseURL("https://localhost:7243/img/game");
+        this.load.image('sky', 'skies/sky.png');
+        this.load.image('bg-mountain', 'sprites/bg-mountain.png'); // 932x183
+        this.load.image('bg-tree', 'sprites/bg-tree.png'); // 519x197
+        this.load.image('character', 'sprites/character.png');
+        this.load.image('ground', 'sprites/ground.png'); // 579x84
+        this.load.image('long-tree', 'sprites/long-tree.png');
+        this.load.image('tree_4', 'sprites/two-tree.png');
+
+        for (var i = 1; i <= 4; i++)
+            this.load.image('cloud_' + i, 'sprites/cloud_' + i + '.png');
+
+        for (var j = 1; j <= 2; j++)
+            this.load.image('grass_' + j, 'sprites/grass_' + j + '.png');
+
+        for (var k = 1; k <= 3; k++)
+            this.load.image('tree_' + k, 'sprites/tree_' + k + '.png');
+    }
+
+    create() {
+        this.add.image(640, this.height / 2, 'sky');
+
+        this.addRandomImages('cloud', 4, 4, 0, this.width, 0, 200);
+        this.add.tileSprite(640, this.height / 2, this.width, 182, 'bg-mountain');
+        this.add.tileSprite(640, this.height * 0.7, this.width, 197, 'bg-tree');
+        this.addRandomImages('tree', 6, 4, 0, this.width, this.height * 0.55, this.height * 0.58);
+        this.addRandomImages('grass', 6, 2, 0, this.width, this.height - 110, this.height);
+        this.add.tileSprite(640, this.height - 42, this.width, 84, 'ground');
+        this.add.image(100, this.height - 280, 'long-tree');
+        this.add.image(1000, this.height - 280, 'long-tree');
+
+
+        //var platforms = this.physics.add.staticGroup();
+        //platforms.create(0, this.height - 42, 'ground');
+    }
+
+    update() {
+    }
+
+    randomPosition(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    randomImg(name, max) {
+        var result = name + '_' + (Math.floor(Math.random() * max) + 1);
+        console.log(result);
+        return result;
+    }
+
+    addRandomImages(name, count, total, minX, maxX, minY, maxY) {
+        for (var i = 0; i < count; i++)
+            this.add.image(this.randomPosition(minX, maxX), this.randomPosition(minY, maxY), this.randomImg(name, total));
+    }
+}
+
+function startGame() {
+
+    const config = {
+        type: Phaser.AUTO,
+        width: 1280,
+        height: 500,
+        scene: SparcGame,
+        parent: 'game',
+        physics: {
+            default: 'arcade'
+        }
+    };
+
+    const game = new Phaser.Game(config);
+}
