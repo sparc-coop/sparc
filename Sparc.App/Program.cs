3a42cf97bff82a46a36b46b4c936f3fdc671d8a0
Joseph Young <joseph@kuviocreative.com>
2026-01-27 11:25:54 -0600

    Make sparc engine url dependent on environment

--- Sparc.App/Program.cs
+++ Sparc.App/Program.cs
@@ -4,8 +4,8 @@ using Sparc.Blossom;
 using Sparc.Blossom.Data;
 
 var builder = BlossomApplication.CreateBuilder<Html>(args);
-builder.AddBlossomEngine("https://localhost:7185/");
-//builder.AddBlossomEngine("https://sparcengine-spaces-e8aba2etcmchcsce.centralus-01.azurewebsites.net/");
+
+builder.AddBlossomEngine(builder.Configuration["SparcEngine"]);
 builder.Services.AddCosmos<SparcContext>(builder.Configuration);
 
 await builder.Build().RunAsync<Html>();
