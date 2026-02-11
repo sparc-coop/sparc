8a5e641da2642d89602e525244b8ff21169ea504
Joseph Young <joseph@josephayoung.com>
2026-02-11 12:27:01 -0600

    Show quest length in UI; adjust styles and Hint alpha
    Added quest length display (in ly) to quest list items when available. Increased grid gap and styled aside in SpaceQuests.razor.css. Set Hint rectangle alpha to 0 for transparency in Hint.js.

--- Sparc.App/wwwroot/SpaceGame/starfield/components/Hint.js
+++ Sparc.App/wwwroot/SpaceGame/starfield/components/Hint.js
@@ -1,6 +1,6 @@
 ﻿export default class Hint extends Phaser.GameObjects.Rectangle {
     constructor(scene, obj) {
-        super(scene, scene.x(obj.x), scene.y(obj.y), 8, 8, 0x008000);
+        super(scene, scene.x(obj.x), scene.y(obj.y), 8, 8, 0x008000, 0);
 
         console.log('adding answer', obj);
 
