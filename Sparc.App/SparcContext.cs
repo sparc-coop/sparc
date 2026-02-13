b004af2aa47f62c78326332d67d5b70782a10cce
Joseph Young <joseph@josephayoung.com>
2026-02-13 15:31:27 -0600

    v4 architecture rewrite, clean build

--- Sparc.App/SparcContext.cs
+++ Sparc.App/SparcContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Sparc.App.Community;
 using Sparc.App.Projects;
 
 namespace Sparc.App;
@@ -8,10 +7,6 @@ public class SparcContext(DbContextOptions options) : DbContext(options)
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.Entity<SparcPost>().ToContainer("TextContent")
-            .HasPartitionKey(x => x.Domain)
-            .HasKey(x => x.Id);
-
         builder.Entity<Project>().ToContainer("Projects")
             .HasPartitionKey(x => x.ProjectId)
             .HasKey(x => x.Id);
