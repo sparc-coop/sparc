0786143ee4da2f35c92f72bace01e0d4e6e3660e
Joseph Young <joseph@josephayoung.com>
2026-03-11 09:05:33 -0500

    Screaming architecture

--- Sparc.App/Program.cs
+++ Sparc.App/Program.cs
@@ -1,11 +1,8 @@
 using Sparc.App;
-using Sparc.App.Layout;
 using Sparc.Blossom;
-using Sparc.Blossom.Data;
 
 var builder = BlossomApplication.CreateBuilder<Html>(args);
 
 builder.AddSparcEngine(builder.Configuration["SparcEngine"]);
-builder.Services.AddCosmos<SparcContext>(builder.Configuration);
 
 await builder.Build().RunAsync<Html>();
--- Sparc.App/Projects/Tag.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sparc.App.Projects;
-
-public class Tag(string id, string name, string type)
-{
-    public string Id { get; set; } = id;
-    public string Name { get; set; } = name;
-    public string Type { get; set; } = type;
-}
