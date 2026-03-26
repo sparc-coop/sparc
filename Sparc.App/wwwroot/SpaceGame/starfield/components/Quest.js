f9248013ebd42821fe3ba773a9cf0268592a73be
Joseph Young <joseph@josephayoung.com>
2026-03-26 08:44:34 -0500

    Calculate all minima as Quest

--- Sparc.App/wwwroot/SpaceGame/starfield/components/Quest.js
+++ Sparc.App/wwwroot/SpaceGame/starfield/components/Quest.js
@@ -1,29 +1,8 @@
-﻿export default class Quest extends Phaser.GameObjects.Line {
+﻿export default class Quest extends Phaser.GameObjects.Sprite {
     constructor(scene, obj) {
-        super(scene, 0, 0, 0, 0, 0, 0, 0xffffff, 0.1);
-
-        this.x = this.baseX(obj);
-        this.y = this.baseY(obj);
-        this.setOrigin(0, 0);
-        this.setLineWidth(2);
-        this.setDepth(3);
-        this.setInteractive().on('pointerdown', () => this.scene.dotnet.invokeMethodAsync('SelectGameObject', obj.id));
-
-        this.updateFromObject(obj);
+        super(scene, scene.x(obj), scene.y(obj), 'Post');
+        this.setScale(scene.z(obj) * 4);
+        this.setAlpha(scene.z(obj));
         scene.add.existing(this);
-    }
-
-    baseX(obj) {
-        return this.scene.x(this.scene.gameState.self);
-    }
-
-    baseY(obj) {
-        return this.scene.y(this.scene.gameState.self);
-    }
-
-    updateFromObject(obj) {
-        var x2 = this.scene.x(obj, 0);
-        var y2 = this.scene.y(obj, 0);
-        this.setTo(x2, y2);
     }
 }
